fa7d4e3fbfe92eb3357260e20071c20b384fc61c
deiby0710 <deibyalejandro10@hotmail.com>
2025-07-12 12:35:23 -0500

    Falta hacer que los loadingAlert se cierren cuando se deje la page

--- frontend/src/pages/PatientForm.jsx
+++ frontend/src/pages/PatientForm.jsx
@@ -1,4 +1,4 @@
-import React, { use, useState } from 'react';
+import React, { useState } from 'react';
 import { SearchForm } from '../components/PatientSearch/SearchForm';
 import { RadioOptionList } from '../components/PatientSearch/RadioOptionList';
 import { defaultAlert } from '../utils/alert';
