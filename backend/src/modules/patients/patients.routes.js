75b4fef4a6181493289aa3a60765f8d77b08ca90
deiby0710 <deibyalejandro10@hotmail.com>
2025-11-07 15:07:34 -0500

    Replace main content with production code

--- backend/src/modules/patients/patients.routes.js
+++ backend/src/modules/patients/patients.routes.js
@@ -1,5 +1,10 @@
 import { Router } from 'express';
-import { validatePatientController, validatorPatientController, getPatientBasicDataController, copayAmount2Controller } from './patients.controller.js';
+import { 
+    validatePatientController,
+    validatorPatientController, 
+    getPatientBasicDataController
+    // copayAmount2Controller
+ } from './patients.controller.js';
 import authMiddleware from '../../middlewares/auth.middleware.js'
 
 const router = Router();
@@ -7,6 +12,6 @@ const router = Router();
 router.post("/validar", authMiddleware, validatePatientController);
 router.post("/consultaAfiliado", authMiddleware, validatorPatientController);
 router.get("/basicData", authMiddleware, getPatientBasicDataController);
-router.post("/copago", authMiddleware, copayAmount2Controller);
+// router.post("/copago", authMiddleware, copayAmount2Controller);
 
 export default router;
