132df8aa3f60801f5a2e5632d9234581b85f04cf
deiby0710 <deibyalejandro10@hotmail.com>
2025-11-12 11:45:56 -0500

    base de datos de usuarios checked

--- backend/src/modules/login/login.service.js
+++ backend/src/modules/login/login.service.js
@@ -1,16 +1,21 @@
 import jwt from 'jsonwebtoken';
-import fs from "fs";
+import pool from '../../../config/db.js';
 
 export const loginS = async(username, password) => {
-    const users = JSON.parse(fs.readFileSync('src/data/users.json', 'utf-8'));
+    const [rows] = await pool.execute(
+        'SELECT username, password, sede FROM usuarios WHERE username = ?',
+        [username]
+    );
     
-    const user = users.find(u => u.username === username)
+    const user = rows[0];
+    
     if(!user){
         throw new Error('USUARIO_NO_ENCONTRADO')
     }
     if(password !== user.password){
         throw new Error('PASSWORD_INCORRECTA')
     }
+
     const token = jwt.sign(
         { username: user.username, sede: user.sede },
         process.env.JWT_SECRET,
